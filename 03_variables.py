22703cc86a5d4dfaac986b95eec036dd061e013b
Robert Vári <mail.robertvari@gmail.com>
2022-02-19 15:43:47 +0100

    update

---
+++ 03_variables.py
@@ -0,0 +1,21 @@
+# define a variable
+
+# store data in memory
+my_name = "Vári Róbert"
+
+
+# get data from memory
+print(my_name)
+print(my_name)
+print(my_name)
+
+# override my_name variable
+my_name = "Kiss Csaba"
+print(my_name)
+print(my_name)
+print(my_name)
+print(my_name)
+
+
+# end of program
+# delete my_name from memory
