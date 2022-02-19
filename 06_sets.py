f0f7f0e16cf81b57951c5405a9bc89635f56e825
Robert Vári <mail.robertvari@gmail.com>
2022-02-19 16:54:08 +0100

    update

---
+++ 06_sets.py
@@ -0,0 +1,12 @@
+# halmazok
+numbers1 = {1, 2, 3, 4}
+numbers2 = {3, 4, 5, 6}
+
+# intersection
+#print(numbers1 & numbers2)
+
+# difference
+print(numbers1 - numbers2)
+
+# symmetrical difference
+print(numbers1 ^ numbers2)
