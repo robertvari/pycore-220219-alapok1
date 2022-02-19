643a083a6322079594dc0ade1c4bd7b0fb099f43
Robert Vári <mail.robertvari@gmail.com>
2022-02-19 15:22:59 +0100

    update

--- 02_string_methods.py
+++ 02_string_methods.py
@@ -2,3 +2,5 @@ print("Hello Python".upper())
 print("HeLLo PyTHon".lower())
 print("heLLo python".title())
 
+print("Hello Python".replace("l", "X"))
+print("Hello Python".replace("Python", "World"))
