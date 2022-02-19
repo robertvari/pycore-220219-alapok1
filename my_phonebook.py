5eba95eea62e0f907e4b3a2b9efd6ec2ef64c781
Robert Vári <mail.robertvari@gmail.com>
2022-02-19 18:08:40 +0100

    update

--- my_phonebook.py
+++ my_phonebook.py
@@ -1,3 +1,6 @@
+import json
+
+
 name = input("What is your name?")
 email = input("Email?")
 phone = input("Phone?")
@@ -15,11 +18,11 @@ print(phonebook)
 
 # todo save data to file
 
-with open("my_data.txt", "w") as my_textfile:
-    my_textfile.write(str(phonebook))
+with open("my_data.json", "w") as f:
+    json.dump(phonebook, f)
 
 
 # load file and print it's content
-with open("my_data.txt") as my_textfile:
-    my_phonebook = my_textfile.read()
-    my_phonebook[phone]
+with open("my_data.json") as f:
+    my_phonebook = json.load(f)
+    print(my_phonebook[phone])
