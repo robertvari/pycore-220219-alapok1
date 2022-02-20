0293b382cc74d5d356c94041cdfc14a9cd8cda4b
Robert Vári <mail.robertvari@gmail.com>
2022-02-20 16:55:13 +0100

    project init

--- PhotoToExcel/PhotoToExcel.py
+++ PhotoToExcel/PhotoToExcel.py
@@ -8,3 +8,11 @@ assert os.path.exists(photo_folder), "This folder does not exist!"
 
 # check if path == folder
 assert os.path.isdir(photo_folder), "Path must be a folder."
+
+
+# todo list folder for content os.listdir()
+
+# todo filter result. Only accept .jpg files photo_files = []
+
+# todo loop through file list
+    # todo collect exif data from photo
