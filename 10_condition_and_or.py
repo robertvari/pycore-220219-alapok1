a1dd7f291e4cd7c757b6eb75217a36f513c109a4
Robert Vári <mail.robertvari@gmail.com>
2022-02-20 14:45:28 +0100

    update

--- 10_condition_and_or.py
+++ 10_condition_and_or.py
@@ -1,6 +1,12 @@
 username = "rober"
-password = "testpas123"
+password = "testpas12"
 
-#           False                   True
+#           True                   True
 if username == "robert" and password == "testpas123":
+    print("Wellcome robert you are logged in")
+else:
+    print("username or password was not correct")
+
+#          True                   False
+if username == "robert" or password == "testpas123":
     print("Wellcome robert you are logged in")
