3632ce6a96d8b9a4e4bf572c07d376f718faab46
Robert Vári <mail.robertvari@gmail.com>
2022-02-20 14:41:22 +0100

    update

---
+++ 10_condition_and_or.py
@@ -0,0 +1,6 @@
+username = "rober"
+password = "testpas123"
+
+#           False                   True
+if username == "robert" and password == "testpas123":
+    print("Wellcome robert you are logged in")
