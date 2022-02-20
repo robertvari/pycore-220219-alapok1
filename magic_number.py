3b93db3ede2d244b0822eeaa2a1d40f3108a0651
Robert Vári <mail.robertvari@gmail.com>
2022-02-20 15:32:14 +0100

    update

---
+++ magic_number.py
@@ -0,0 +1,14 @@
+# game constants
+min_number = 1
+max_number = 10
+max_tries = 3
+
+# random number
+magic_number = 5
+
+# show game intro
+print("="*50, "MAGIC NUMBER", "="*50)
+
+# f = formatted string
+print(f"There is number between {min_number} and {max_number}. Can you guess it?")
+print(f"You have {max_tries} tries.")
