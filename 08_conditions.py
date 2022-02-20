4a103cd31b8d5e3894391578df5492b35aacbb0a
Robert Vári <mail.robertvari@gmail.com>
2022-02-20 14:25:36 +0100

    update

--- 08_conditions.py
+++ 08_conditions.py
@@ -1,7 +1,17 @@
-number = 10
+number = 30
 
 #    True
+# if number == 10:
+#     print("Number = 10")
+# else:  # not True
+#     print("Number is not 10")
+
+# if/elif
 if number == 10:
     print("Number = 10")
-else:  # not True
-    print("Number is not 10")
+elif number == 20:
+    print("Number = 20")
+elif number == 30:
+    print("Number = 30")
+else:
+    print("Number is not 10, 20 or 30")
